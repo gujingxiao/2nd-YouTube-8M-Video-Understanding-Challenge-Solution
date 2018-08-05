7790f480513574b4f68da0e0da79508908738722
seigato <gujingxiao@hotmail.com>
2018-08-05 20:43:29 +0800

    add MultiEnsembleChainModel

--- video_level_models.py
+++ video_level_models.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 # Copyright 2016 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,19 +29,19 @@ flags.DEFINE_integer("moe_num_mixtures", 3,
     "The number of mixtures (excluding the dummy 'expert') used for MoeModel.")
 
 # DeepChainModel
-flags.DEFINE_integer("deep_chain_layers", 3,
+flags.DEFINE_integer("deep_chain_layers", 2,
     "The number of layers used for DeepChainModel，建议使用3,4")
-flags.DEFINE_integer("deep_chain_relu_cells", 1024,
+flags.DEFINE_integer("deep_chain_relu_cells", 768,
     "The number of relu cells used for DeepChainModel，建议使用256,512,1024")
 
 # MultiTaskDeepChainModel
-flags.DEFINE_integer("chain_layers_1", 3,
+flags.DEFINE_integer("chain_layers_1", 2,
     "The number of layers used for DeepChainModel，建议使用3, 4")
-flags.DEFINE_integer("chain_elu_cells", 896,
+flags.DEFINE_integer("chain_elu_cells", 672,
     "The number of relu cells used for DeepChainModel，建议大于256")
 flags.DEFINE_integer("chain_layers_2", 2,
     "The number of layers used for DeepChainModel，建议使用2, 3")
-flags.DEFINE_integer("chain_leaky_relu_cells", 896,
+flags.DEFINE_integer("chain_leaky_relu_cells", 672,
     "The number of relu cells used for DeepChainModel，建议大于256")
 
 
@@ -120,6 +121,51 @@ class MoeModel(models.BaseModel):
                                      [-1, vocab_size])
     return {"predictions": final_probabilities}
 
+class DeepConcatModel(models.BaseModel):
+  """A softmax over a mixture of logistic models (with L2 regularization)."""
+
+  def create_model(self, model_input, vocab_size, num_mixtures=None,
+                   l2_penalty=1e-8, sub_scope="", original_input=None,
+                   dropout=False, keep_prob=None, **unused_params):
+
+    num_layers = FLAGS.deep_chain_layers
+    relu_cells = FLAGS.deep_chain_relu_cells
+
+    next_input = model_input
+    for layer in range(num_layers):
+      sub_prediction = self.sub_model(next_input, 512, num_mixtures=2, sub_scope=sub_scope+"prediction-%d"%layer,
+                                      dropout=dropout, keep_prob=keep_prob)
+      sub_activation = slim.fully_connected(sub_prediction, 768, activation_fn=None,
+          weights_regularizer=slim.l2_regularizer(l2_penalty), scope=sub_scope+"relu-%d"%layer)
+
+      sub_relu = tf.nn.leaky_relu(sub_activation)
+      relu_norm = tf.nn.l2_normalize(sub_relu, axis=1)
+      next_input = tf.concat([next_input, relu_norm], axis=1)
+
+    main_predictions = self.sub_model(next_input, vocab_size, num_mixtures=3, sub_scope=sub_scope+"-main")
+
+    return {"predictions": main_predictions}
+
+  def sub_model(self, model_input, vocab_size, num_mixtures=None,
+                l2_penalty=1e-8, sub_scope="",
+                dropout=False, keep_prob=None, **unused_params):
+    num_mixtures = num_mixtures
+
+    if dropout:
+      model_input = tf.nn.dropout(model_input, keep_prob=keep_prob)
+
+    gate_activations = slim.fully_connected(model_input, vocab_size * (num_mixtures + 1), activation_fn=None,
+        biases_initializer=None, weights_regularizer=slim.l2_regularizer(l2_penalty), scope="gates-"+sub_scope)
+    expert_activations = slim.fully_connected(model_input, vocab_size * num_mixtures, activation_fn=None,
+        weights_regularizer=slim.l2_regularizer(l2_penalty), scope="experts-"+sub_scope)
+
+    gating_distribution = tf.nn.softmax(tf.reshape(gate_activations, [-1, num_mixtures + 1]))
+    expert_distribution = tf.nn.sigmoid(tf.reshape(expert_activations, [-1, num_mixtures]))
+
+    final_probabilities_by_class_and_batch = tf.reduce_sum(gating_distribution[:, :num_mixtures] * expert_distribution, 1)
+    final_probabilities = tf.reshape(final_probabilities_by_class_and_batch, [-1, vocab_size])
+    return final_probabilities
+
 class DeepCombineChainModel(models.BaseModel):
   """A softmax over a mixture of logistic models (with L2 regularization)."""
 
@@ -133,7 +179,7 @@ class DeepCombineChainModel(models.BaseModel):
     next_input = model_input
     support_lists = []
     for layer in range(num_layers):
-      sub_prediction = self.sub_model(next_input, 1536, num_mixtures=2, sub_scope=sub_scope+"prediction-%d"%layer,
+      sub_prediction = self.sub_model(next_input, 768, num_mixtures=2, sub_scope=sub_scope+"prediction-%d"%layer,
                                       dropout=dropout, keep_prob=keep_prob)
       sub_activation = slim.fully_connected(sub_prediction, relu_cells, activation_fn=None,
           weights_regularizer=slim.l2_regularizer(l2_penalty), scope=sub_scope+"relu-%d"%layer)
@@ -143,11 +189,11 @@ class DeepCombineChainModel(models.BaseModel):
       next_input = tf.concat([next_input, relu_norm], axis=1)
       support_lists.append(sub_prediction)
 
-    main_predictions = self.sub_model(next_input, vocab_size, num_mixtures=4, sub_scope=sub_scope+"-main")
+    main_predictions = self.sub_model(next_input, vocab_size, num_mixtures=3, sub_scope=sub_scope+"-main")
 
     support_lists = tf.stack(support_lists, axis=1)
     support_activations = tf.reduce_mean(support_lists, axis=1)
-    support_predictions = self.sub_model(support_activations, vocab_size, num_mixtures=4, sub_scope=sub_scope+"-support")
+    support_predictions = self.sub_model(support_activations, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-support")
 
     return {"predictions": main_predictions, "support_predictions": support_predictions}
 
@@ -189,7 +235,7 @@ class MultiTaskCombineChainModel(models.BaseModel):
     support_lists_1 = []
     support_lists_2 = []
     for layer in range(num_layers_1):
-      sub_prediction_1 = self.sub_model(next_input_1, 896, num_mixtures=2, sub_scope=sub_scope+"prediction1-%d"%layer,
+      sub_prediction_1 = self.sub_model(next_input_1, 512, num_mixtures=2, sub_scope=sub_scope+"prediction1-%d"%layer,
                                       dropout=dropout, keep_prob=keep_prob)
       sub_activation_1 = slim.fully_connected(sub_prediction_1, elu_cells, activation_fn=None,
           weights_regularizer=slim.l2_regularizer(l2_penalty), scope=sub_scope+"elu-%d"%layer)
@@ -199,13 +245,13 @@ class MultiTaskCombineChainModel(models.BaseModel):
       next_input_1 = tf.concat([next_input_1, elu_norm], axis=1)
       support_lists_1.append(sub_prediction_1)
 
-    main_predictions_1 = self.sub_model(next_input_1, vocab_size, num_mixtures=3, sub_scope=sub_scope+"-main1")
+    main_predictions_1 = self.sub_model(next_input_1, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-main1")
     support_lists_1 = tf.stack(support_lists_1, axis=1)
     support_activations_1 = tf.reduce_mean(support_lists_1, axis=1)
     support_predictions_1 = self.sub_model(support_activations_1, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-support1")
 
     for layer in range(num_layers_2):
-      sub_prediction_2 = self.sub_model(next_input_2, 896, num_mixtures=2, sub_scope=sub_scope+"prediction2-%d"%layer,
+      sub_prediction_2 = self.sub_model(next_input_2, 736, num_mixtures=2, sub_scope=sub_scope+"prediction2-%d"%layer,
                                       dropout=dropout, keep_prob=keep_prob)
       sub_activation_2 = slim.fully_connected(sub_prediction_2, leaky_relu_cells, activation_fn=None,
           weights_regularizer=slim.l2_regularizer(l2_penalty), scope=sub_scope+"leakyrelu-%d"%layer)
@@ -215,7 +261,82 @@ class MultiTaskCombineChainModel(models.BaseModel):
       next_input_2 = tf.concat([next_input_2, leakyrelu_norm], axis=1)
       support_lists_2.append(sub_prediction_2)
 
-    main_predictions_2 = self.sub_model(next_input_2, vocab_size, num_mixtures=3, sub_scope=sub_scope+"-main2")
+    main_predictions_2 = self.sub_model(next_input_2, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-main2")
+    support_lists_2 = tf.stack(support_lists_2, axis=1)
+    support_activations_2 = tf.reduce_mean(support_lists_2, axis=1)
+    support_predictions_2 = self.sub_model(support_activations_2, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-support2")
+
+
+    return {"predictions": main_predictions_1, "support_predictions": support_predictions_1, "predictions2": main_predictions_2, "support_predictions2": support_predictions_2}
+
+  def sub_model(self, model_input, vocab_size, num_mixtures=None,
+                l2_penalty=1e-8, sub_scope="",
+                dropout=False, keep_prob=None, **unused_params):
+    num_mixtures = num_mixtures
+
+    if dropout:
+      model_input = tf.nn.dropout(model_input, keep_prob=keep_prob)
+
+    gate_activations = slim.fully_connected(model_input, vocab_size * (num_mixtures + 1), activation_fn=None,
+        biases_initializer=None, weights_regularizer=slim.l2_regularizer(l2_penalty), scope="gates-"+sub_scope)
+    expert_activations = slim.fully_connected(model_input, vocab_size * num_mixtures, activation_fn=None,
+        weights_regularizer=slim.l2_regularizer(l2_penalty), scope="experts-"+sub_scope)
+
+    gating_distribution = tf.nn.softmax(tf.reshape(gate_activations, [-1, num_mixtures + 1]))
+    expert_distribution = tf.nn.sigmoid(tf.reshape(expert_activations, [-1, num_mixtures]))
+
+    final_probabilities_by_class_and_batch = tf.reduce_sum(gating_distribution[:, :num_mixtures] * expert_distribution, 1)
+    final_probabilities = tf.reshape(final_probabilities_by_class_and_batch, [-1, vocab_size])
+    return final_probabilities
+
+
+class MultiEnsembleChainModel(models.BaseModel):
+  """A softmax over a mixture of logistic models (with L2 regularization)."""
+
+  def create_model(self, model_input1, model_input2, vocab_size, num_mixtures=None,
+                   l2_penalty=1e-8, sub_scope="", original_input=None,
+                   dropout=False, keep_prob=None, **unused_params):
+
+    num_layers_1 = FLAGS.chain_layers_1
+    elu_cells = FLAGS.chain_elu_cells
+    num_layers_2 = FLAGS.chain_layers_2
+    leaky_relu_cells = FLAGS.chain_leaky_relu_cells
+
+    next_input_1 = model_input1  # softdbof_netvlad_activation
+    next_input_2 = model_input2  # gatedbof_netfv_activation
+    support_lists_1 = []
+    support_lists_2 = []
+
+    # softdbof_netvlad_activation
+    for layer in range(num_layers_1):
+      sub_prediction_1 = self.sub_model(next_input_1, 512, num_mixtures=2, sub_scope=sub_scope+"prediction1-%d"%layer,
+                                      dropout=dropout, keep_prob=keep_prob)
+      sub_activation_1 = slim.fully_connected(sub_prediction_1, elu_cells, activation_fn=None,
+          weights_regularizer=slim.l2_regularizer(l2_penalty), scope=sub_scope+"elu-%d"%layer)
+
+      sub_elu = tf.nn.elu(sub_activation_1)
+      elu_norm = tf.nn.l2_normalize(sub_elu, dim=1)
+      next_input_1 = tf.concat([next_input_1, elu_norm], axis=1)
+      support_lists_1.append(sub_prediction_1)
+
+    main_predictions_1 = self.sub_model(next_input_1, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-main1")
+    support_lists_1 = tf.stack(support_lists_1, axis=1)
+    support_activations_1 = tf.reduce_mean(support_lists_1, axis=1)
+    support_predictions_1 = self.sub_model(support_activations_1, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-support1")
+
+    # gatedbof_netfv_activation
+    for layer in range(num_layers_2):
+      sub_prediction_2 = self.sub_model(next_input_2, 512, num_mixtures=2, sub_scope=sub_scope+"prediction2-%d"%layer,
+                                      dropout=dropout, keep_prob=keep_prob)
+      sub_activation_2 = slim.fully_connected(sub_prediction_2, leaky_relu_cells, activation_fn=None,
+          weights_regularizer=slim.l2_regularizer(l2_penalty), scope=sub_scope+"leakyrelu-%d"%layer)
+
+      sub_leakyrelu = tf.nn.leaky_relu(sub_activation_2)
+      leakyrelu_norm = tf.nn.l2_normalize(sub_leakyrelu, dim=1)
+      next_input_2 = tf.concat([next_input_2, leakyrelu_norm], axis=1)
+      support_lists_2.append(sub_prediction_2)
+
+    main_predictions_2 = self.sub_model(next_input_2, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-main2")
     support_lists_2 = tf.stack(support_lists_2, axis=1)
     support_activations_2 = tf.reduce_mean(support_lists_2, axis=1)
     support_predictions_2 = self.sub_model(support_activations_2, vocab_size, num_mixtures=2, sub_scope=sub_scope+"-support2")
